a7a54234ac0e912d80df727b2618edaaf3fa36d2
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-06 21:22:13 +0200

    Setup script created
    The setup script was created to follow best practices conventions in python coding.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='login_manager',
+   version='1.0',
+   description='A login Manager Play Tool',
+   author='Kevin Smith',
+   author_email='kevinsmith.kis@gmail.com',
+   packages=['login_manager'],
+   install_requires=[],
+)
