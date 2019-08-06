af24e7426519365d5d4d86ac55c3e621a4cab02b
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-06 21:24:08 +0200

    Database helper script improvements
    Parameter safety comes first, thus the script and thus relevant sql select, insert and creation of table was made safer according to best practices.

--- login_manager/helper.py
+++ login_manager/helper.py
@@ -11,11 +11,19 @@ def create_db_table():
 
 
 def create_user_db(user):
+    """ SQL Insert statement that preserves parameter safety"""
+    keys = ','.join(user.keys())
+    ques = ','.join(list('?'*len(user)))
+    values = tuple(user.values())
     curs = conn.cursor()
-    curs.execute("""INSERT INTO users(first_name,last_name,username,email,phone_number,password) 
-    VALUES ('{first_name}','{last_name}','{username}','{email}','{phone_number}','{password}')""".format(**user))
+    curs.execute('INSERT INTO users ('+keys+') VALUES ('+ques+')', values)
     conn.commit()
 
 
-def find_user_db():
+def find_user_db(username):
+    """ SQL Select statement that preserves parameter safety"""
+    print("username = [%s]" % username)
     curs = conn.cursor()
+    curs.execute('SELECT password FROM users WHERE username =?;', (username,))
+    print(curs.fetchone())
+    return curs.fetchone()
