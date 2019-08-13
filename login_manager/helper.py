855a6e4db7e91b5272b148e7360afc08ba8637c1
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-13 22:20:52 +0200

    Added popup message
    added a popup message when attempting to insert empty strings as the new user credentials. The popup message is only part of the effect.

--- login_manager/helper.py
+++ login_manager/helper.py
@@ -1,4 +1,21 @@
+from tkinter import Tk, Label, Button
+
 
 def raise_above_all(master):
     master.attributes('-topmost', True)
     master.focus_force()
+
+
+def popup_message():
+    popup = Tk()
+    popup.wm_title("Error")
+    popup.geometry("250x150+525+150")
+    raise_above_all(popup)
+
+    message_lable = Label(popup, text="--Dumbass--\n\nYou have not populated\n all the required data fields.\n "
+                                      "Did you get confused?\n")
+    message_lable.pack(side='top')
+
+    ok_button = Button(popup, text="Yes, I Apologise", command=popup.destroy)
+    ok_button.pack()
+    popup.mainloop()
